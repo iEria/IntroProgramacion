c564f6042de3dc0837a88951e20c6af15430f6e5
Eria <corderosteven280@gmail.com>
2023-05-20 20:58:52 -0600

    Comentario prueba

---
+++ SC202_2C2023_G1/src/main/java/com/mycompany/sc202_2c2023_g1/SC202_2C2023_G1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sc202_2c2023_g1;
+
+/**
+ *
+ * @author eria
+ */
+public class SC202_2C2023_G1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
