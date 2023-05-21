b51dcc07c62a30bf3da57bafe3807e4a046c0f74
iEria <corderosteven280@gmail.com>
2023-05-21 01:50:29 -0600

    Inicio Menu

--- SC202_2C2023_G1/src/main/java/com/mycompany/sc202_2c2023_g1/SC202_2C2023_G1.java
+++ SC202_2C2023_G1/src/main/java/com/mycompany/sc202_2c2023_g1/SC202_2C2023_G1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.sc202_2c2023_g1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author eria
@@ -11,6 +13,48 @@ package com.mycompany.sc202_2c2023_g1;
 public class SC202_2C2023_G1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean repetir = true;
+        while(repetir == true){
+            String opcion = JOptionPane.showInputDialog("---Menu Parqueo---\n"
+                                                    + "1.Opcion #1     \n"
+                                                    + "2.Opcion #2     \n"
+                                                    + "3.Opcion #3     \n");
+        if (ValidacionNumero(opcion) == true){
+            switch (opcion) { 
+                case "1":
+                    JOptionPane.showMessageDialog(null,"Opcion #1");
+                    repetir = false;
+                break;
+                case "2":
+                    JOptionPane.showMessageDialog(null,"Opcion #2");
+                    repetir = false;
+                    break;
+                case "3":
+                    JOptionPane.showMessageDialog(null,"Opcion #3");
+                    repetir = false;
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"No es una opcion, reintente");
+                    repetir = true;
+    
+                }
+            }else{
+                JOptionPane.showMessageDialog(null,"No es numero , reintente");
+                repetir = true;
+            }
+        }
+        
     }
+    
+    public static boolean ValidacionNumero(String opcion)
+    {
+        try{
+            Integer.parseInt(opcion);
+            return true;
+        }catch(NumberFormatException e){
+            return false;
+        }
+    }
+    
+    
 }
