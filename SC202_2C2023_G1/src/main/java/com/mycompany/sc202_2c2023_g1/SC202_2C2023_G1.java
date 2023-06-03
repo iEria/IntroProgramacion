c22960576ec2d320b53c947ecaac8fe320ad5d46
iEria <corderosteven280@gmail.com>
2023-06-03 12:42:28 -0600

    Correcion Menu , no permitia salir

--- SC202_2C2023_G1/src/main/java/com/mycompany/sc202_2c2023_g1/SC202_2C2023_G1.java
+++ SC202_2C2023_G1/src/main/java/com/mycompany/sc202_2c2023_g1/SC202_2C2023_G1.java
@@ -1,60 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.sc202_2c2023_g1;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author eria
- */
 public class SC202_2C2023_G1 {
 
     public static void main(String[] args) {
         boolean repetir = true;
-        while(repetir == true){
+        while (repetir) {
             String opcion = JOptionPane.showInputDialog("---Menu Parqueo---\n"
-                                                    + "1.Opcion #1     \n"
-                                                    + "2.Opcion #2     \n"
-                                                    + "3.Opcion #3     \n");
-        if (ValidacionNumero(opcion) == true){
-            switch (opcion) { 
-                case "1":
-                    JOptionPane.showMessageDialog(null,"Opcion #1");
-                    repetir = false;
+                    + "1.Opcion #1     \n"
+                    + "2.Opcion #2     \n"
+                    + "3.Opcion #3     \n");
+            if (opcion == null) {
+                // Si se presiona el botón de cerrar o cancelar, se termina el ciclo
                 break;
-                case "2":
-                    JOptionPane.showMessageDialog(null,"Opcion #2");
-                    repetir = false;
-                    break;
-                case "3":
-                    JOptionPane.showMessageDialog(null,"Opcion #3");
-                    repetir = false;
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null,"No es una opcion, reintente");
-                    repetir = true;
-    
+            }
+            if (ValidacionNumero(opcion)) {
+                switch (opcion) {
+                    case "1":
+                        JOptionPane.showMessageDialog(null, "Opcion #1");
+                        break;
+                    case "2":
+                        JOptionPane.showMessageDialog(null, "Opcion #2");
+                        break;
+                    case "3":
+                        JOptionPane.showMessageDialog(null, "Opcion #3");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "No es una opción válida, reintente");
+                        repetir = true;
                 }
-            }else{
-                JOptionPane.showMessageDialog(null,"No es numero , reintente");
+            } else {
+                JOptionPane.showMessageDialog(null, "No es un número, reintente");
                 repetir = true;
             }
         }
-        
     }
-    
-    public static boolean ValidacionNumero(String opcion)
-    {
-        try{
+
+    public static boolean ValidacionNumero(String opcion) {
+        try {
             Integer.parseInt(opcion);
             return true;
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return false;
         }
     }
-    
-    
 }
